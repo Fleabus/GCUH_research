cd56e70cd51aead43a06b1b73d7c62cb6739a96f
Matthew Lee <matthew.lee-mattner@griffithuni.edu.au>
2017-07-11 15:40:34 +1000

    Equalize and shuffle implemented

--- __main__.py
+++ __main__.py
@@ -3,4 +3,8 @@ from setup import setup_data
 if __name__ == "__main__":
     data_type = input("What lead? (e.g. MLII, V1, V2...):\n")
     data_formatter = setup_data(data_type.upper())
+    data_formatter.counter()
+    data_formatter.shuffle()
+    data_formatter.counter()
     data_formatter.equalize_data()
+    data_formatter.counter()
--- data_formatter.py
+++ data_formatter.py
@@ -7,6 +7,12 @@ class Data_Formatter:
         self.x = x_data
         self.y = y_data
 
+    def shuffle(self):
+        s = np.arange(self.x.shape[0])
+        np.random.shuffle(s)
+        self.x = self.x[s]
+        self.y = self.y[s]
+
     # Ensures all classifications are of equal length
     def equalize_data(self):
         x_values = []
@@ -22,5 +28,33 @@ class Data_Formatter:
             index_point = np.argmax(self.y[i])
             x_values[index_point].append(self.x[i])
             y_values[index_point].append(self.y[i])
-        print(len(x_values))
-        print(len(x_values[0]), len(x_values[1]))
+        self.x = []
+        self.y = []
+        # Find the minimum amount of data points in a category
+        minAmount = -1
+        for i in range(classifications):
+            if(len(x_values[i]) < minAmount or minAmount == -1):
+                minAmount = len(x_values[i])
+        # Cycle through all classifications and reduce to minAmount
+        self.x = x_values[0:classifications][0:minAmount]
+        self.y = y_values[0:classifications][0:minAmount]
+        # Assign back to numpy arrays for processing
+        self.x = np.array(self.x)
+        self.y = np.array(self.y)
+        # Reshape into standard format [total, steps]
+        '''
+        This currently does not support more than 2 classifications
+        '''
+        self.x = np.concatenate((self.x[0], self.x[1]), axis=0)
+        self.y = np.concatenate((self.y[0], self.y[1]), axis=0)
+
+    def counter(self):
+        normal_counter = 0
+        abnormal_counter = 0
+        for i in range(len(self.y)):
+            if(self.y[i][0] == 1):
+                normal_counter = normal_counter + 1
+            if(self.y[i][1] == 1):
+                abnormal_counter = abnormal_counter + 1
+        print(normal_counter)
+        print(abnormal_counter)
