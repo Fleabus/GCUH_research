b796c75f14dde4cabd58abd0c76d558dc29c547a
Aidan <mcaidans@gmail.com>
2017-08-22 10:32:38 +1000

    nnclass good

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -7,40 +7,44 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 class NeuralNetwork:
     shape = []
 
-    def __init__(self, shape):
-        self.shape = shape
-        self.x = tf.placeholder(tf.float32, [None, self.shape[0]]) #Creates an input placeholder with input of 360 (signal length as 1d Array)
-        self.y_ = tf.placeholder(tf.float32, [None, self.shape[-1]])
-        return
+    def __init__(self):
+        self.self = self
 
-    def setupModel(self, sess):
+    def setupModel(self, sess, shape):
         self.sess = sess
-        self.y = self.setupHLs()
+        self.shape = shape
+        self.x = tf.placeholder(tf.float32, [None, self.shape[0][0]]) #Creates an input placeholder with input of 360 (signal length as 1d Array)
+        self.y_ = tf.placeholder(tf.float32, [None, self.shape[-1][0]])
+        self.y = self.getY(shape)
+
         self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
         self.train_step = tf.train.GradientDescentOptimizer(0.1).minimize(self.cross_entropy)
         self.correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
         self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+
         sess.run(tf.global_variables_initializer())
 
-    def setupHLs(self):
-        inputValue = self.x
-        for i in range(0, len(self.shape)-1):
-            inputValue = self.hidden_layer(inputValue, [self.shape[i], self.shape[i+1]])
-        return inputValue
+    def getY(self, shape):
+        output = self.x
+        for i in range(1, len(self.shape)-1):
+            output = self.hidden_layer(output, shape[i])
+        return output
 
-    def weight_variable(self, shape):
-        W = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+    def hidden_layer(self, output, layerShape):
+        w = self.weight_variable(layerShape)
+        b = self.bias_variable(layerShape)
+        output = tf.matmul(output, w) + b
+        return output
+
+    def weight_variable(self, layerShape):
+        W = tf.Variable(tf.truncated_normal(layerShape, stddev=0.1))
         return W
 
-    def bias_variable(self, shape):
-        b = tf.Variable(tf.constant(0.1, shape=[shape]))
+    def bias_variable(self, layerShape):
+        b = tf.Variable(tf.constant(0.1, shape=[layerShape[1]]))
         return b
 
-    def hidden_layer(self, inputValue, layerShape):
-        w = self.weight_variable([layerShape[0], layerShape[1]])
-        b = self.bias_variable(layerShape[1])
-        output = tf.matmul(inputValue, w) + b
-        return output
+
 
     def train(self, x, y):
         self.sess.run(self.train_step, feed_dict={self.x: x, self.y_: y})
@@ -51,7 +55,7 @@ class NeuralNetwork:
 
     def run(self, x):
         print(x)
-        print(sess.run([self.y], feed_dict={self.x: [0,x]}))
+        #print(sess.run([self.y], feed_dict={self.x: [0,x]}))
 
 
 #MAIN#######################################
@@ -71,10 +75,14 @@ df.split_training_testing()
 print("ytest", df.countType(df.y_test))
 print("ytrain", df.countType(df.y_train))
 
-nn = NeuralNetwork([360, 100, 2])
+nn = NeuralNetwork()
 with tf.Session() as sess:
-    nn.setupModel(sess)
+    nn.setupModel(sess, [[360], [360, 100], [2]])
+    '''
     nn.test(df.x_test, df.y_test)
     nn.train(df.x_train, df.y_train)
     nn.test(df.x_test, df.y_test)
     nn.run(df.x_test[0])
+    saver = tf.train.Saver()
+    saver.save(sess, "model/ann")
+    '''
--- run_unlabelled.py
+++ run_unlabelled.py
@@ -2,33 +2,37 @@ from plot_signal import plot_signal
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-filename1 = "unlabelled/1131314/new9.txt"
+filename1 = "sah/3022_4.asc"
 #filename = "unlabelled/2987_2.asc"
 #filename = "unlabelled/4264/waves4.asc"
-filename2 = "unlabelled/2987_5.asc"
+filename2 = "sah/2987_2.asc"
 
 with open(filename1) as f:
     content1 = f.readlines()
 with open(filename2) as f:
     content2 = f.readlines()
 
+content1.pop(0)
+content1.pop(0)
 content2.pop(0)
 content2.pop(0)
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content1 = [float(x.replace('\\n', '').strip()) for x in content1]
 content2 = [float(x.replace('\\n', '').strip()) for x in content2]
-x, y, z = range(10000), np.array(np.concatenate((content1[2000:7000], content2[2000:7000]), axis=0)), np.ones(10000)
+x, y, z = range(1000), np.array(np.concatenate((content1[200:700], content2[200:700]), axis=0)), np.ones(1000)
 #y = np.array([n+np.mean(y) for n in y])
 #y = np.array([(n--1)/(5--1) for n in y])
 print(y)
-n = np.lib.pad(y, (900, 900), 'edge')
+n = np.lib.pad(y, (90, 90), 'edge')
 temp = []
-for i in range(len(n)-900):
-    next_x = n[i:i+900]
+for i in range(len(n)-90):
+    next_x = n[i:i+90]
     temp.append((next_x - np.min(next_x))/(np.max(next_x)-np.min(next_x)))
 temp = np.array(temp)
-
+'''
 sess = tf.Session()
 
 saver = tf.train.import_meta_graph('model/ann.meta')
@@ -45,9 +49,9 @@ output = [(n--0.3)/(0.3--0.3) for n in output]
 z = np.zeros(len(y))
 output = np.array(output)
 
-for i in range(len(n)-1800):
-    z[i] =  np.mean(output[i:i+900], axis=0)
-
+for i in range(len(n)-180):
+    z[i] =  np.mean(output[i:i+90], axis=0)
+'''
 plt.style.use('dark_background')
 plot_signal(x, y, z)
 plt.show()
